cbe372729d9b9452631aac4f10fbeac88008a9f0
Juan Manuel Castro <manuelca@yahoo.com>
2023-01-21 03:45:03 +0000

    FE updated to send 1 sencond record to BE, BE process with Deep Speech

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ let ws;
 let mediaRecorder;
 let stream;
 const micButton = document.getElementById("mic-button");
+const wsURL = "wss://8000-gosftw-streamstt-kcwtzvs8gko.ws-us83.gitpod.io/ws";
+let textOutput = document.getElementById("text-output").innerHTML;
+//const wsURL = "wss://34257-gosftw-streamstt-kcwtzvs8gko.ws-us83.gitpod.io/ws";
+
 let recording = false;
 let recordingInterval;
 
-navigator.mediaDevices.getUserMedia({ audio: true })
+/* navigator.mediaDevices.getUserMedia({ audio: true })
   .then(mediaStream => {
     stream = mediaStream;
     micButton.addEventListener("click", function() {
@@ -25,7 +29,7 @@ navigator.mediaDevices.getUserMedia({ audio: true })
           recording = false;
         } else {
           console.log("START RECORDING");
-          ws = new WebSocket('ws://localhost:5005/ws');
+          ws = new WebSocket(wsURL);
           stream = mediaStream;
           mediaRecorder = new MediaRecorder(stream);
           mediaRecorder.start();
@@ -33,14 +37,20 @@ navigator.mediaDevices.getUserMedia({ audio: true })
           recording = true;
           ws.onopen = () => {
                 recordingInterval = setInterval(() => {
+                  console.log("hummm");
                     mediaRecorder.ondataavailable = event => {
+                        let audioData= new Float32Array(event.data);
                         console.log("tic");
-                        ws.send(event.data);
+                        console.log(audioData);
+                        if(audioData.length > 0) {
+                          console.log("audioData > 0")
+                        }
+                        ws.send(audioData);
+                        ws.onmessage = function(event) {
+                            console.log("tac");
+                            document.getElementById("text-output").innerHTML = event.data;
+                        }
                     };
-                    ws.onmessage = function(event) {
-                        console.log("tac");
-                        document.getElementById("text-output").innerHTML = event.data;
-                    }
                 }, 1000);
           };
         }
@@ -49,3 +59,103 @@ navigator.mediaDevices.getUserMedia({ audio: true })
   .catch(error => {
     console.error('Error accessing microphone: ', error);
   });
+
+*/
+const batchSize = 10;
+
+
+const startSTT = () => {
+    const socket = new WebSocket(wsURL);
+    let intervalId;
+    
+    let options = {
+        audioBitsPerSecond : 16000,
+        mimeType : 'audio/webm; codecs=opus'
+    }
+
+    // open connection
+    socket.addEventListener('open', function() {
+        navigator.mediaDevices.getUserMedia({
+            audio: true
+        }).then(function (stream) {
+            console.log("socket open");
+            mediaRecorder = new MediaRecorder(stream, options);
+            mediaRecorder.start(1000);
+            mediaRecorder.ondataavailable = event => {
+                console.log("ondataavailable", event.data);
+                //[event.data].toBlob((blob) => socket.send(blob), 'audio/wav');
+                //#let data = new Int16Array(event.data);
+                let data = new Blob([event.data], { type: 'audio/webm; codecs=opus' });
+                //let data = btoa(event.data);
+                //console.log(data);
+                socket.send(data);
+                /*const audioBlob = new Blob([event.data], { type: 'audio/ogg' });
+                const audioUrl = URL.createObjectURL(audioBlob);
+                const audioPlayer = document.getElementById('audioPlayer');
+                audioPlayer.src = audioUrl;
+                audioPlayer.play()*/
+                /*let audioBlob = new Blob([event.data], { type: 'audio/wav' });
+                socket.send(event.data);*/
+            }
+            /*intervalId = setInterval(() => {
+                console.log("setInterval");
+                mediaRecorder.ondataavailable = event => {
+                    console.log("ondataavailable");
+                    socket.send(event.data);
+                    // let audioData= new Blob(event.data, { type: "audio/ogg; codecs=opus" });
+                    //socket.send(event.data);
+                    audioData.push(event.data);
+                    //socket.send(audio_bytes);
+                    if (audioData.length === batchSize) {
+                        console.log("Send data");
+                        //const audioBlob = new Blob(audioData);
+                        let audioBytes = new Float32Array(audioData).buffer;
+                        socket.send(audioBytes);
+                        audioData = [];
+                    }
+                }
+                
+            }, 1000);*/
+        });
+    });
+
+    // Listen for messages
+    socket.addEventListener('message', function (event) {
+        console.log("message received: ", event.data);
+        document.getElementById("text-output").innerHTML = event.data;
+    });
+
+    // Stop the interval and video reading on close
+    socket.addEventListener('close', function () {
+        console.log("connection clossed");
+        clearInterval(intervalId);
+        if (mediaRecorder.state === 'recording') {
+            mediaRecorder.stop();
+        }
+    });
+
+    return socket;
+};
+
+
+
+let socket;
+
+micButton.addEventListener("click", function() {
+    
+    if (recording) {
+        micButton.innerHTML = '<i class="fas fa-microphone"></i>';
+        if (socket) {
+            socket.close();
+        }
+        
+        recording = false;
+    } else {
+        
+        micButton.innerHTML = '<i class="fas fa-stop"></i>';
+        socket = startSTT();
+        
+        recording = true;
+    }
+    
+});
--- ws/app.py
+++ ws/app.py
@@ -1,24 +1,48 @@
 from fastapi import FastAPI
 from fastapi.websockets import WebSocket, WebSocketDisconnect
 from datetime import datetime
+import base64
+import wave
+
+import deepspeech
+import numpy as np
 
 app = FastAPI()
 
+# Initialize the DeepSpeech model
+model_path = "deepspeech-0.9.3-models.pbmm"
+scorer_path = "deepspeech-0.9.3-models.scorer"
+model = deepspeech.Model(model_path)
+model.enableExternalScorer(scorer_path)
 
+
+# Create a new WebSocket endpoint
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     try:
-        await websocket.send_text("Connection established!")
-
         while True:
-            data = await websocket.receive_bytes()
+            #audio_bytes = await websocket.receive_bytes()
+            #print(audio_bytes)
+            #print(type(audio_bytes))
+            audio_bytes = await websocket.receive_bytes()
+            offset = len(audio_bytes) % 2
+            #offset = int(np.ceil(len(audio_bytes) / 2) * 2)
+            # Decode the base64 string to bytes
+            
             current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            await websocket.send_text(f"Time: {current_time}")
-    except WebSocketDisconnect:
-        print("Client disconnect")
-
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5005)
+            #with wave.open(audio_bytes, 'rb') as wav_file:
+                # read the audio data and convert it to the desired format
+            #    audio = np.fromstring(wav_file.readframes(-1), dtype=np.int16)
+            # pass the audio data to the DeepSpeech model
+            audio = np.frombuffer(audio_bytes, dtype=np.int16, offset=offset, count=-1)
+            # audio = audio.reshape(-1, 2)
+            # audio = np.resize(audio, (-1, 2))
+            
+            text = model.stt(audio)
+            #text = "model.stt(audio)"
+            print(text)
+            await websocket.send_text(f"Time: {current_time}  {text}")
+    except WebSocketDisconnect as e:
+        # print('connection clossed')
+        pass
